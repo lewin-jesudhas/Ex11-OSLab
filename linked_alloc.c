639bea4e819e342e543260fdca38fb24006751bd
H Lewin Jesudhas <136236663+lewin-jesudhas@users.noreply.github.com>
2024-11-02 09:12:25 +0530

    Update linked_alloc.c

--- linked_alloc.c
+++ linked_alloc.c
@@ -1,65 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void recursiveParts(int pagesAllocation[]) {
-    int s, length, c;
-    
-    printf("Enter the beginning block's index as well as length: ");
-    scanf("%d%d", &s, &length);
-
-    int allocated = 0;
-    
-    if (pagesAllocation[s] == 0) {
-        for (int j = s; j < s + length; j++) {
-            if (j >= 50) {  // Check to avoid going out of bounds
-                printf("Reached end of memory, can't allocate further.\n");
-                break;
-            }
-            if (pagesAllocation[j] == 0) {
-                pagesAllocation[j] = 1;
-                printf("%d------>%d\n", j, pagesAllocation[j]);
-                allocated++;
-            } else {
-                printf("The block %d has already been allocated\n", j);
-            }
-        }
-
-        // If we couldn't allocate the exact number of blocks needed, inform the user
-        if (allocated < length) {
-            printf("Could only allocate %d blocks out of the requested %d.\n", allocated, length);
-        }
-    } else {
-        printf("The starting block %d has already been allocated\n", s);
-    }
-    
-    printf("Do you want to add more files? \n");
-    printf("Enter 1 to continue, Enter 0 to stop: ");
-    scanf("%d", &c);
-    if (c == 1)
-        recursiveParts(pagesAllocation);
-    else
-        return;  // Use return instead of exit(0) to end recursion cleanly
-}
-
 int main() {
-    int pagesAllocation[50], p1, a1;
-    for (int i = 0; i < 50; i++)
-        pagesAllocation[i] = 0;
+    int f[50], p, i, st, len, j, c, k, a;
 
-    printf("Enter the quantity of initially allocated blocks: ");
-    scanf("%d", &p1);
-    printf("Enter the indices of the initially allocated blocks: ");
-    for (int i = 0; i < p1; i++) {
-        scanf("%d", &a1);
-        if (a1 >= 0 && a1 < 50) {
-            pagesAllocation[a1] = 1;
-        } else {
-            printf("Invalid block index %d. Please enter indices between 0 and 49.\n", a1);
-            i--;  // decrement to re-prompt the correct index
-        }
+    // Initialize all blocks as free (0)
+    for(i = 0; i < 50; i++)
+        f[i] = 0;
+
+    printf("Enter how many blocks already allocated: ");
+    scanf("%d", &p);
+
+    printf("Enter blocks already allocated: ");
+    for(i = 0; i < p; i++) {
+        scanf("%d", &a);
+        f[a] = 1;
     }
 
-    recursiveParts(pagesAllocation);
+    while (1) {
+        printf("Enter index starting block and length: ");
+        scanf("%d%d", &st, &len);
+        k = len;
+
+        if (f[st] == 0) {
+            for (j = st; j < (st + k); j++) {
+                if (f[j] == 0) {
+                    f[j] = 1;
+                    printf("%d-------->%d\n", j, f[j]);
+                } else {
+                    printf("%d Block is already allocated \n", j);
+                    k++;
+                }
+            }
+        } else {
+            printf("%d starting block is already allocated \n", st);
+        }
+
+        printf("Do you want to enter more files (Yes - 1 / No - 0): ");
+        scanf("%d", &c);
+        if (c == 0)
+            break;
+    }
 
     return 0;
 }
