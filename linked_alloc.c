b6ab16c9486aaf247d2fd2f695f6685e3caf2d6e
H Lewin Jesudhas <136236663+lewin-jesudhas@users.noreply.github.com>
2024-11-02 08:29:30 +0530

    Update linked_alloc.c

--- linked_alloc.c
+++ linked_alloc.c
@@ -1,44 +1,47 @@
-#include <stdio.h>
-#include <conio.h>
-
-struct file {
-    char fname[10];
-    int start;
-    int size;
-    int block[10];
-} f[10];
-
-int main() {
-    int i, j, n;
-    clrscr();
-    printf("Enter number of files: ");
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++) {
-        printf("Enter file name: ");
-        scanf("%s", f[i].fname);
-        printf("Enter starting block: ");
-        scanf("%d", &f[i].start);
-        f[i].block[0] = f[i].start; // Storing the starting block
-
-        printf("Enter number of blocks: ");
-        scanf("%d", &f[i].size);
-        printf("Enter block numbers: ");
-        for (j = 1; j <= f[i].size; j++) {
-            scanf("%d", &f[i].block[j]);
-        }
-    }
-
-    printf("File\tStart\tSize\tBlock\n");
-    for (i = 0; i < n; i++) {
-        printf("%s\t%d\t%d\t", f[i].fname, f[i].start, f[i].size);
-        for (j = 1; j <= f[i].size - 1; j++) {
-            printf("%d--->", f[i].block[j]);
-        }
-        printf("%d", f[i].block[j]);
-        printf("\n");
-    }
-
-    getch();
-    return 0;
-}
+#include <stdio.h>  
+#include <stdlib.h>  
+void recursiveParts(int pagesAllocation[]){  
+    int s, length, k, c, j;  
+    printf("Enter the beginning block's index as well as length: ");  
+    scanf("%d%d", &s, &length);  
+    k = length;  
+    if (pagesAllocation[s] == 0){  
+        for (j = s; j < (s + k); j++){  
+            if (pagesAllocation[j] == 0){  
+                pagesAllocation[j] = 1;  
+                printf("%d------>%d\n", j, pagesAllocation[j]);  
+            }  
+            else {  
+                printf("The block %d has already been allocated \n", j);  
+                k++;  
+            }  
+        }  
+    }  
+    else  
+        printf("The block %d has already been allocated \n", s);  
+    printf("Do you want to add more files? \n");  
+    printf("Enter 1 for continue, Enter 0 for No: ");  
+    scanf("%d", &c);  
+    if (c==1)  
+        recursiveParts(pagesAllocation);  
+    else  
+        exit(0);  
+    return;  
+}  
+  
+int main(){  
+    int pagesAllocation[50], p1, a1;  
+    for (int i = 0; i < 50; i++)  
+        pagesAllocation[i] = 0;  
+    printf("Enter the quantity of provided blocks: ");  
+    scanf("%d", &p1);  
+    printf("Enter the number of alloted blocks ");  
+    for (int i = 0; i < p1; i++){  
+        scanf("%d", &a1);  
+        pagesAllocation[a1] = 1;  
+    }  
+  
+    recursiveParts(pagesAllocation);  
+  
+    return 0;  
+}  
